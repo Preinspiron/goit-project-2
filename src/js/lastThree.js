018ddeb99cb2738e93835ae3df36ce9da3a24d53
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-04-19 22:24:37 +0200

    replace style

--- src/js/lastThree.js
+++ src/js/lastThree.js
@@ -1,9 +1,34 @@
 import Marvel from './fetchMarvel';
 import Handlebars from '../hbs/lastThreeComics.hbs';
 import HBSmodal from '../hbs/_modalComics.hbs';
-import HBSCharacters from '../hbs/_modalComics_Characters.hbs';
 import refs from './refs';
-import '../images/svg/sprite.svg';
+// import sliderGalleryComics from './lastComics';
+// console.log(sliderGalleryComics);
+
+function sliderGalleryComics() {
+  // const slidesContainer = document.getElementById('slides-container');
+  // const slide = document.querySelector('.slide');
+  //   // const prevButton = document.getElementById('slide-arrow-prev');
+  //   // const nextButton = document.getElementById('slide-arrow-next');
+  // refs.refresh();
+  refs['#slide-arrow-next'].addEventListener('click', () => {
+    console.log(1);
+    const slideWidth = refs['.slide'].clientWidth;
+
+    refs['.slides-container'].scrollLeft += slideWidth;
+    console.dir(refs['.slides-container']);
+  });
+
+  refs['#slide-arrow-prev'].addEventListener('click', () => {
+    const slideWidth = refs['.slide'].clientWidth;
+    refs['.slides-container'].scrollLeft -= slideWidth;
+    console.log(2);
+  });
+}
+
+// }
+
+// console.log(4);
 
 Marvel.getLastThreeComics('/comics', {
   params: {
@@ -13,20 +38,32 @@ Marvel.getLastThreeComics('/comics', {
 })
   .then(res => {
     const respJSONed = res.data.data.results;
+    // console.log(response.data.results);
+    // console.log(respJSONed);
     console.log(respJSONed);
     const hbsObjRandom = respJSONed.map(element => ({
       title: element.title,
       id: element.id,
-      urlImg:
-        element.images[0].path +
-        '/portrait_incredible' +
-        '.' +
-        element.images[0].extension,
+      urlImg: element.images[0].path + '.' + element.images[0].extension,
       creators: element.creators.items[0].name,
     }));
+    // console.log('111', hbsObjRandom);
+    // console.log(Handlebars(hbsObjRandom));
     refs.IndexLastThreeComics.innerHTML = Handlebars(hbsObjRandom);
+    // // console.log('data', hbsObjRandom);
+    // const newRes = Object.entries(response.data.results);
+    // console.log(newRes);
+
+    // return hbsObjRandom;
+    // sliderGalleryComics();
+    // console.log(res);
   })
-  .then(console.log('then'))
+  .then(() => {
+    console.log(refs);
+    refs.refresh();
+    console.log(refs);
+    sliderGalleryComics();
+  })
   .catch(res => console.dir(res));
 
 refs.indexLastComics.addEventListener('click', onClickLastComics, {
@@ -36,89 +73,56 @@ refs.indexLastComics.addEventListener('click', onClickLastComics, {
 
 function onClickLastComics(e) {
   refs.indexComicsModal.classList.remove('is-hidden');
-  document.body.style.overflowY = 'hidden';
-  refs.refresh();
-
-  console.log('hiden');
-  if (e.target.nodeName === 'IMG')
-    Marvel.getComicById(e.target.dataset.id)
-      .then(
-        ({
-          data: {
-            data: { results },
-          },
-        }) => results[0]
-      )
-      .then(res => {
-        console.log(res);
-        return res;
+  Marvel.getComicById(e.target.dataset.id)
+    .then(
+      ({
+        data: {
+          data: { results },
+        },
+      }) => results[0]
+    )
+    .then(res => {
+      console.log(res);
+      return res;
+    })
+    .then(
+      ({
+        id,
+        title,
+        prices,
+        format,
+        thumbnail: { path, extension },
+        ups,
+        digitalId,
+        pageCount,
+        creators,
+        dates,
+      }) => ({
+        id,
+        title,
+        prices: prices[0].price,
+        format,
+        creatorsName: creators.items[0].name,
+        creatorsRole: creators.items[0].role,
+        // thumbnail,
+        path,
+        extension,
+        ups,
+        digitalId,
+        pageCount,
+        dates: new Date(dates[1].date).getFullYear(),
       })
-      .then(
-        ({
-          id,
-          title,
-          prices,
-          format,
-          thumbnail: { path, extension },
-          ups,
-          digitalId,
-          pageCount,
-          creators,
-          dates,
-        }) => ({
-          id,
-          title,
-          prices: prices[0].price,
-          format,
-          creatorsName: creators.items[0].name,
-          creatorsRole: creators.items[0].role,
-          // thumbnail,
-          path,
-          extension,
-          ups,
-          digitalId,
-          pageCount,
-          dates: new Date(dates[1].date).getFullYear(),
-        })
-      )
-      .then(res => {
-        refs.indexComicsModal.innerHTML = HBSmodal(res);
-        return res;
-      })
-      .then(res => {
-        refs.refresh();
-        refs['#modalCloseBtn'].addEventListener('click', onComicsModalClose);
-        return res;
-      })
-      .then(innerModalLinkToCharacter)
-      .catch(console.log);
+    )
+    .then(res => {
+      refs.indexComicsModal.innerHTML = HBSmodal(res);
+      return res;
+    })
+    .then(console.log)
+    .then(() => {
+      // console.log('theb');
+      // console.log('close');
+    });
+  // console.log('111');
 }
 
-const onComicsModalClose = e => {
-  refs.indexComicsModal.classList.add('is-hidden');
-  document.body.style.overflowY = 'auto';
-  refs.indexComicsModal.innerHTML = '';
-  console.log('click');
-  refs['#modalCloseBtn'] = null;
-};
-
-async function innerModalLinkToCharacter({ digitalId }) {
-  const pullCharactersById = await Marvel.getComicsCharactersById(digitalId);
-  const {
-    data: {
-      data: { results },
-    },
-  } = pullCharactersById;
-  console.log(pullCharactersById);
-  const characters = results.map(
-    ({ thumbnail: { path, extension }, name }) => ({ path, extension, name })
-  );
-  console.log(characters);
-  refs['.modal-comics-characters .items'].insertAdjacentHTML(
-    'afterbegin',
-    HBSCharacters(characters)
-  );
-  console.log(refs['.modal-comics-characters .items']);
-
-  console.log(pullCharactersById);
-}
+// console.log(1);
